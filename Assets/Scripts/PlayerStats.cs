73dc46fa63a0f3c4ea50d929f8d13124f11a38ca
Leo Yin <71328966+leoyin1127@users.noreply.github.com>
2023-03-18 18:53:48 -0400

    Create PlayerStats.cs
    add health, mana and shielding bar.
    add take damage, use mana, add shielding function

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -1,47 +1,85 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerStats : MonoBehaviour
 {
-    public int maxHealth = 10;
-    public int maxMana = 10;
-    public int maxShield = 10;
+    public Slider healthBar;
+    public Slider manaBar;
+    public Slider shieldingBar;
 
-    public int health;
-    public int mana;
-    public int shield;
+    public float maxHealth = 5;
+    public float maxMana = 100;
+    public float maxShielding = 5;
+
+    private float currentHealth;
+    private float currentMana;
+    private float currentShielding;
 
     void Start()
     {
-        health = maxHealth;
-        mana = maxMana;
-        shield = maxShield;
+        currentHealth = maxHealth;
+        currentMana = maxMana;
+        currentShielding = maxShielding;
+
+        UpdateBars();
     }
-
-    public void TakeDamage(int damage)
-    {
-
-        if (shield > 0)
-        {
-            shield -= damage;
-        }
-
-        else
-        {
-            health -= damage;
-
-            if (health <=0 )
-            {
-                Destroy(gameObject);
-            }
-        }
-
-    }
-
-    // Update is called once per frame
     void Update()
     {
+        //test if takedamage function could work
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            TakeDamage(1);
+            
+        }
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            UseMana(20);
+            
+        }
+    }
+    void UpdateBars()
+    {
+        healthBar.maxValue = maxHealth;
+        healthBar.value = currentHealth;
+
+        manaBar.maxValue = maxMana;
+        manaBar.value = currentMana;
+
+        shieldingBar.maxValue = maxShielding;
+        shieldingBar.value = currentShielding;
+    }
+
+    public void TakeDamage(float damage)
+    {
+       
+        if (currentHealth > 0)
+        {
+            if (currentShielding > 0)
+            {
+                currentShielding -= damage;
+            }
+                else
+                {
+                    currentHealth -= damage;
+                    Debug.Log("00000000000");
+                }
+        }
         
+        UpdateBars();
+    }
+
+    public void UseMana(float manaCost)
+    {
+        if (currentMana > 0)
+        {
+            currentMana -= manaCost;
+        }
+        UpdateBars();
+    }
+
+    public void AddShielding(float shieldAmount)
+    {
+        currentShielding += shieldAmount;
+        UpdateBars();
     }
 }
